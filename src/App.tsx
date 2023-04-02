afc77a298ae0142de92fb6065c5075cf4ac68029
Anthony Zhu <anthonyzhu132@gmail.com>
2023-04-02 16:55:10 -0700

    added ability to add new cells on click of span

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,17 @@ function App() {
 
   const changeCellContentValue = (value: string, index: number) => {
     //Loop through old cells array, match selected index with array index and replace value
-    setCells((prevCellsArray) => prevCellsArray.map((cell, i) => (i === index ? value : cell)))
+    setCells((prevCellsArray) =>
+      prevCellsArray.map((cell, i) => (i === index ? value : cell))
+    );
+  };
+
+  const onPlusClicked = (index: number) => {
+    setCells((prevCellsArray) => [
+      ...prevCellsArray.slice(0, index + 1),
+      "_",
+      ...prevCellsArray.slice(index + 1),
+    ]);
   };
 
   return (
@@ -15,9 +25,14 @@ function App() {
         {cells.map((cell, index) => (
           <div key={index} className="cell">
             <input
-              onChange={(e) => changeCellContentValue(e.currentTarget.value, index)}
+              onChange={(e) =>
+                changeCellContentValue(e.currentTarget.value, index)
+              }
               value={cell}
             />
+            {index < cells.length - 1 && (
+              <span onClick={() => onPlusClicked(index)}>+</span>
+            )}
           </div>
         ))}
       </div>
