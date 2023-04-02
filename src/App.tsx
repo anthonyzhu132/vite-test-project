e35bd78cf5ecb2a886e4cb20e9a7648dcf3cb042
Anthony Zhu <anthonyzhu132@gmail.com>
2023-04-02 16:41:05 -0700

    added input to change array value

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,24 @@ import "./App.css";
 function App() {
   const [cells, setCells] = useState(["a", "b", "c"]);
 
+  const changeCellContentValue = (value: string, index: number) => {
+    //Loop through old cells array, match selected index with array index and replace value
+    setCells((prevCellsArray) => prevCellsArray.map((cell, i) => (i === index ? value : cell)))
+  };
+
   return (
-    <div className="App">
-      {cells.map((cell) => (
-        <div className="cell">{cell}</div>
-      ))}
-    </div>
+    <main className="App">
+      <div className="cell-container">
+        {cells.map((cell, index) => (
+          <div key={index} className="cell">
+            <input
+              onChange={(e) => changeCellContentValue(e.currentTarget.value, index)}
+              value={cell}
+            />
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
 
